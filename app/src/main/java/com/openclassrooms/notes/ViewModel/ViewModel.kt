6d3c70fdcd84f4ad4021d4c5900d5f77b82846e1
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-04-17 15:04:50 +0200

    backup

--- app/src/main/java/com/openclassrooms/notes/ViewModel/ViewModel.kt
+++ app/src/main/java/com/openclassrooms/notes/ViewModel/ViewModel.kt
@@ -1,30 +1,15 @@
 package com.openclassrooms.notes.ViewModel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
 import com.openclassrooms.notes.repository.NotesRepository
 import com.openclassrooms.notes.widget.NotesAdapter
 import kotlinx.coroutines.launch
 
-public class NotesViewModel() : ViewModel() {
+class NotesViewModel() : ViewModel() {
 
     private val notesRepository = NotesRepository()
-    private val notesAdapter = NotesAdapter(emptyList())
 
-    /**
-     * Collects notes from the repository and updates the adapter.
-     */
-    public fun collectNotes() {
-        viewModelScope.launch {
-            notesRepository.notes.collect {
-                notesAdapter.updateNotes(it)
-            }
-        }
-    }
+    val notes = notesRepository.notes
 
 }
