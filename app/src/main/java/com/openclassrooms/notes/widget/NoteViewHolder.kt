6216e36cb715f5efda342d646a442f9cff14f62e
OlivierMarteaux <olivier_marteaux@hotmail.com>
2025-04-17 11:52:18 +0200

    03_20250417_OM: data model Note implemented in source code (#1)

--- app/src/main/java/com/openclassrooms/notes/widget/NoteViewHolder.kt
+++ app/src/main/java/com/openclassrooms/notes/widget/NoteViewHolder.kt
@@ -2,6 +2,7 @@ package com.openclassrooms.notes.widget
 
 import androidx.recyclerview.widget.RecyclerView
 import com.openclassrooms.notes.databinding.NoteBinding
+import com.openclassrooms.notes.model.Note
 
 /**
  * A view holder for displaying a note in a RecyclerView.
@@ -13,9 +14,9 @@ class NoteViewHolder(private val binding: NoteBinding): RecyclerView.ViewHolder(
      * Binds the view holder to a note.
      * @param note The note to bind to the view holder.
      */
-    fun bind(note: Pair<String, String>) {
-        binding.title.text = note.first
-        binding.body.text = note.second
+    fun bind(note: Note) {
+        binding.title.text = note.title
+        binding.body.text = note.body
     }
 
 }
--- app/src/main/java/com/openclassrooms/notes/widget/NotesAdapter.kt
+++ app/src/main/java/com/openclassrooms/notes/widget/NotesAdapter.kt
@@ -4,18 +4,19 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.openclassrooms.notes.databinding.NoteBinding
+import com.openclassrooms.notes.model.Note
 
 /**
  * An adapter for displaying a list of notes in a RecyclerView.
  * @param notes The list of notes to display.
  */
-class NotesAdapter(private var notes: List<Pair<String, String>>) : RecyclerView.Adapter<NoteViewHolder>() {
+class NotesAdapter(private var notes: List<Note>) : RecyclerView.Adapter<NoteViewHolder>() {
 
     /**
      * Updates the list of notes displayed by the adapter.
      * @param newNotes The new list of notes to display.
      */
-    fun updateNotes(newNotes: List<Pair<String, String>>) {
+    fun updateNotes(newNotes: List<Note>) {
         notes = newNotes
         notifyDataSetChanged()
     }
