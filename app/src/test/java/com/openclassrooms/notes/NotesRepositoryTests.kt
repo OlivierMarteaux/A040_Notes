08a8fdc5671df57842fb7d745e08947253a1bc3f
Olivier Marteaux <olivier_marteaux@hotmail.com>
2025-04-17 16:06:32 +0200

    backup

--- app/src/test/java/com/openclassrooms/notes/NotesRepositoryTests.kt
+++ app/src/test/java/com/openclassrooms/notes/NotesRepositoryTests.kt
@@ -1,26 +1,24 @@
 package com.openclassrooms.notes
 
 import com.openclassrooms.notes.repository.NotesRepository
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class NotesRepositoryTest {
 
     @Test
-    fun notesFlow_emitsPredefinedNotes() = runTest {
-        // Given
+    fun notesRepository_notesFlowCollected_emitsPredefinedNotes() = runTest {
+        // Given: the NotesRepository with predefined notes
         val notesRepository = NotesRepository()
 
-        // When
+        // When: the notes flow is collected
         val result = notesRepository.notes.first()
 
-        // Then
+        // Then: the flow emits the predefined notes
         assertEquals(10, result.size)
         assertEquals("La vie est belle", result[0].title)
-        assertEquals("Risez et amusez-vous.", result[9].title)
+        assertEquals("Riez et amusez-vous.", result[9].title)
     }
 }
