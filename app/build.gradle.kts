986cc4fd120e379fc414158e3e3effaaee740e08
OlivierMarteaux <olivier_marteaux@hotmail.com>
2025-04-17 15:33:48 +0200

    04_20250417_OM: ViewModel creation and implementation (#2)
    * add dependendies for viewmodel.
    * compileSdk 33>34
    * create the viewmodel and implement it in MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.openclassrooms.notes"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.openclassrooms.notes"
@@ -40,12 +40,19 @@ android {
 
 dependencies {
 
+    val lifecycleVersion = "2.8.7"
+    val activityVersion = "1.9.0"
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.recyclerview:recyclerview:1.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+    implementation("androidx.activity:activity-ktx:$activityVersion")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/openclassrooms/notes/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/notes/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.openclassrooms.notes
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.openclassrooms.notes.ViewModel.NotesViewModel
 import com.openclassrooms.notes.databinding.ActivityMainBinding
-import com.openclassrooms.notes.repository.NotesRepository
 import com.openclassrooms.notes.widget.NoteItemDecoration
 import com.openclassrooms.notes.widget.NotesAdapter
 import kotlinx.coroutines.launch
@@ -20,27 +21,47 @@ class MainActivity : AppCompatActivity() {
      */
     private lateinit var binding: ActivityMainBinding
 
+    /**
+     * An adapter for displaying a list of notes in a RecyclerView.
+     */
     private val notesAdapter = NotesAdapter(emptyList())
 
-    private val notesRepository = NotesRepository()
+//    private val notesRepository = NotesRepository()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /** Create a ViewModel the first time the system calls an activity's onCreate() method.
+         *  Re-created activities receive the same ViewModel instance created by the first activity.
+         *  Use the 'by viewModels()' Kotlin property delegate from the activity-ktx artifact
+         */
+        val notesViewModel: NotesViewModel by viewModels()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         initRecyclerView()
         initFABButton()
-        collectNotes()
+        collectNotes(notesViewModel)
     }
 
+//    /**
+//     * Collects notes from the repository and updates the adapter.
+//     */
+//    private fun collectNotes() {
+//        lifecycleScope.launch {
+//            notesRepository.notes.collect {
+//                notesAdapter.updateNotes(it)
+//            }
+//        }
+//    }
+
     /**
-     * Collects notes from the repository and updates the adapter.
+     * Collects notes from the viewModel and updates the adapter.
      */
-    private fun collectNotes() {
+    private fun collectNotes(notesViewModel: NotesViewModel) {
         lifecycleScope.launch {
-            notesRepository.notes.collect {
+            notesViewModel.notes.collect {
                 notesAdapter.updateNotes(it)
             }
         }
@@ -70,10 +91,7 @@ class MainActivity : AppCompatActivity() {
                     resources.getInteger(R.integer.span_count)
                 )
             )
-
             adapter = notesAdapter
         }
-
     }
-
 }
