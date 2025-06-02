752504a6b74529ce59ab02dc8e7724b49e70dc3b
OlivierMarteaux <olivier_marteaux@hotmail.com>
2025-06-02 14:30:28 +0200

    08_20250602_OM: notes collect method moved from viewModel to mainActivity (#6)

--- app/src/main/java/com/openclassrooms/notes/viewmodel/ViewModel.kt
+++ app/src/main/java/com/openclassrooms/notes/viewmodel/ViewModel.kt
@@ -4,17 +4,21 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.openclassrooms.notes.model.Note
 import com.openclassrooms.notes.repository.NotesRepository
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 class NotesViewModel : ViewModel() {
 
     private val notesRepository = NotesRepository()
-    private lateinit var notes: List<Note>
 
-    fun getNotes(): List<Note> {
-        viewModelScope.launch {
-            notesRepository.notes.collect { notes = it }
-        }
-        return notes
-    }
+    val notes: Flow<List<Note>> = notesRepository.notes
+
+//    private lateinit var notes: List<Note>
+
+//    fun getNotes(): List<Note> {
+//        viewModelScope.launch {
+//            notesRepository.notes.collect { notes = it }
+//        }
+//        return notes
+//    }
 }
